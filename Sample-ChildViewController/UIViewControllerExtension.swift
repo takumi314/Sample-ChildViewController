8511d47afb921b74b5f7678931dd5426016899a2
takumi314 <isospin_symmetry@me.com>
2018-05-30 00:54:07 +0900

    [feat] extend UIViewController

---
+++ Sample-ChildViewController/UIViewControllerExtension.swift
@@ -0,0 +1,13 @@
+//
+//  UIViewControllerExtension.swift
+//  Sample-ChildViewController
+//
+//  Created by NishiokaKohei on 2018/05/30.
+//  Copyright © 2018年 Takumi. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+}
