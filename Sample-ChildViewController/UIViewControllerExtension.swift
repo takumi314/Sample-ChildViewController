b6526da76e841ecd302b69d846130fc19bb7aefb
takumi314 <isospin_symmetry@me.com>
2018-05-30 01:01:37 +0900

    [feat] define methods

--- Sample-ChildViewController/UIViewControllerExtension.swift
+++ Sample-ChildViewController/UIViewControllerExtension.swift
@@ -9,5 +9,21 @@
 import UIKit
 
 extension UIViewController {
-    
+
+    func add(_ child: UIViewController) {
+        addChildViewController(child)
+        view.addSubview(child.view)
+        didMove(toParentViewController: self)
+    }
+
+    func remove() {
+        guard let _ = self.parent else {
+            return
+        }
+        
+        willMove(toParentViewController: nil)
+        removeFromParentViewController()
+        view.removeFromSuperview()
+    }
+
 }
