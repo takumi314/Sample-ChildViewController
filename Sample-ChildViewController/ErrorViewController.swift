ef5ad0481e5141b5a86ce521331df9ec68bc023d
takumi314 <isospin_symmetry@me.com>
2018-05-30 01:29:22 +0900

    [WIP] make new VIewController for error

---
+++ Sample-ChildViewController/ErrorViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ErrorViewController.swift
+//  Sample-ChildViewController
+//
+//  Created by NishiokaKohei on 2018/05/30.
+//  Copyright © 2018年 Takumi. All rights reserved.
+//
+
+import UIKit
+
+class ErrorViewController: UIViewController {
+
+    var reloadHandler: () -> Void = {}
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
+
+
