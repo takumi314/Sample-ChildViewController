2e2c580ea58f9fc82b96d47206c944402232377c
takumi314 <isospin_symmetry@me.com>
2018-05-30 00:52:39 +0900

    [feat] make new ViewController

---
+++ Sample-ChildViewController/LoadingViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LoadingViewController.swift
+//  Sample-ChildViewController
+//
+//  Created by NishiokaKohei on 2018/05/30.
+//  Copyright © 2018年 Takumi. All rights reserved.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+    private lazy var activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(activityIndicator)
+
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0.0),
+            activityIndicator.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 0.0)
+        ])
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // We use a 0.5 second delay to not show an activity indicator
+        // in case our data loads very quickly.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { [weak self] in
+            self?.activityIndicator.startAnimating()
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
